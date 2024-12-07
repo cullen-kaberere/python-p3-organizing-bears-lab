11bc3de45adc727f3f6a9b50ec0fa64a00a7f769
cullen kaberere <kabesh86@gmail.com>
2024-12-07 04:30:58 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES
+('Mr. Chocolate', 35 , 'M','red','Protective', 1 ),
+('Rowdy', 27, 'M', 'brown', 'Affectionate', 1),
+('Tabitha', 30, 'F', 'white', 'strict', 1),
+('Sergeant Brown', 45, 'M', 'brown', 'altruistic', 0),
+('Melissa', 22, 'F', 'brown', 'playful', 0),
+('Grinch', 38 , 'M', 'black', 'fearful', 1),
+('Wendy', 32, 'F', 'black', 'social', 1 ),
+(NULL , NULL, 'M', 'red', 'attentive', 1);
